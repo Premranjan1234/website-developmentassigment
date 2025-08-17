6352d7ca169ab85991a03b1c3b9b5a49e1b13f84
premtrayistats <learning@trayistats.com>
2025-08-18 00:45:44 +0530

    ...

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/Premranjan1234/website-developmentassigment',
+  base: '/website-developmentassigment/', // ✅ Only path, with leading and trailing slash
   plugins: [
     react(),
     tailwindcss(),
